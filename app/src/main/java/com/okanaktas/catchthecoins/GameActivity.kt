0e8f74de6f0c2b6540720a52de09c67090aaf842
Okan Aktaş <okanaktas02@gmail.com>
2024-11-02 04:19:09 +0300

    feature
    added runnable and handler for countdown

--- app/src/main/java/com/okanaktas/catchthecoins/GameActivity.kt
+++ app/src/main/java/com/okanaktas/catchthecoins/GameActivity.kt
@@ -1,6 +1,7 @@
 package com.okanaktas.catchthecoins
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.okanaktas.catchthecoins.databinding.ActivityGameBinding
@@ -8,12 +9,24 @@ import com.okanaktas.catchthecoins.databinding.ActivityGameBinding
 class GameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGameBinding
     private var score = 0
+    private var countdown = 60
+
+    private var runnable = Runnable {}
+    private var handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+
+        runnable = Runnable {
+            binding.textViewCountDown.text = "Countdown: ${countdown}"
+            countdown--
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.post(runnable)
     }
 
     fun buttonImage(view: View) {
