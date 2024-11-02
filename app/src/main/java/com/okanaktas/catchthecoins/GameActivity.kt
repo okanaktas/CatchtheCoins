3e7354ad9893b00ff3ff7995dfa42087195c45bb
Okan Aktaş <okanaktas02@gmail.com>
2024-11-02 04:32:02 +0300

    feature
    added alertDialog to determine what to do when the game ends

--- app/src/main/java/com/okanaktas/catchthecoins/GameActivity.kt
+++ app/src/main/java/com/okanaktas/catchthecoins/GameActivity.kt
@@ -1,18 +1,22 @@
 package com.okanaktas.catchthecoins
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.okanaktas.catchthecoins.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGameBinding
     private var score = 0
-    private var countdown = 60
+    private var countdown = 2
 
     private var runnable = Runnable {}
-    private var handler = Handler()
+    private var handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +29,30 @@ class GameActivity : AppCompatActivity() {
             binding.textViewCountDown.text = "Countdown: ${countdown}"
             countdown--
             handler.postDelayed(runnable, 1000)
+
+            if (countdown < 0) {
+                handler.removeCallbacks(runnable)
+                showEndDialog()
+            }
         }
         handler.post(runnable)
+    }
+
+    private fun showEndDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Finished Game!")
+        builder.setMessage("Your Score: $score")
+        builder.setPositiveButton("Play Again") { _, _ ->
+            val intent = intent
+            startActivity(intent)
+            finish()
+        }
+        builder.setNegativeButton("Go Home Screen") { _, _ ->
+            val intent = Intent(this@GameActivity, WelcomeActivity::class.java)
+            startActivity(intent)
+        }
+        builder.setCancelable(false) // Dialog'un dışına tıklanarak kapanmasını önlemek için
+        builder.show()
     }
 
     fun buttonImage(view: View) {
